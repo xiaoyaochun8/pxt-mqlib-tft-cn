9bff7a31bd71e7f84fecdaaf657d9f730e1cb8f5
Barry.C <wjyzxhm888@163.com>
2026-03-28 09:54:45 +0800

    更新 pxt.json, main.blocks, main.ts, font16.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
 
+/**
+* 使用此文件来定义自定义函数和图形块。
+* 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
+*/
+
+/**
+ * mqlib blocks
+ */
+//% weight=100 color=#5c2d91 icon=""
+//% groups=['tft彩屏','others']
+namespace mqlib {
+
+    /**
+     * tft显示中文
+     */
+    //% subcategory="tft"
+    //% group='tft彩屏'
+    //% block='tft显示中文'
+    export function tftShowCh(): void {
+        
+    }
+}
